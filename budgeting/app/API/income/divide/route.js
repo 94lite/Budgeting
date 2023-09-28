356d5c935f4376a33b907a9f406f7ee472f41305
Lite W Kim <litek@orionhealth.com>
2023-09-29 10:04:43 +1300

    refactor out utility functions for later reuse

--- budgeting/app/API/income/divide/route.js
+++ budgeting/app/API/income/divide/route.js
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-import { spectrum, days, daysInMonth } from "@/constants/dates";
+import { spectrum } from "@/constants/dates";
 import Expense from "@/constants/expense";
+import { findAPaymentDate, getPrevDate, getNextDate } from "@/constants/payments-utiltity";
 
 export const GET = async (request) => {
   const { searchParams } = request.nextUrl;
@@ -63,90 +64,6 @@ export const GET = async (request) => {
   });
   const response = NextResponse.json(data);
   return response;
-};
-
-export const getPrevDate = (strDate, frequency) => {
-  const date = new Date(strDate);
-  switch (frequency) {
-    case "yearly":
-      date.setFullYear(date.getFullYear() - 1);
-      break;
-    case "monthly":
-      date.setMonth(date.getMonth() - 1);
-      break;
-    case "fortnightly":
-      date.setDate(date.getDate() - 14);
-      break;
-    case "weekly":
-      date.setDate(date.getDate() - 7);
-      break;
-    case "daily":
-      date.setDate(date.getDate() - 1);
-      break;
-    default:
-      return null;
-  }
-  return date;
-};
-
-export const getNextDate = (strDate, frequency) => {
-  const date = new Date(strDate);
-  switch (frequency) {
-    case "yearly":
-      date.setFullYear(date.getFullYear() + 1);
-      break;
-    case "monthly":
-      date.setMonth(date.getMonth() + 1);
-      break;
-    case "fortnightly":
-      date.setDate(date.getDate() + 14);
-      break;
-    case "weekly":
-      date.setDate(date.getDate() + 7);
-      break;
-    case "daily":
-      date.setDate(date.getDate() + 1);
-      break;
-    default:
-      return null;
-  }
-  return date;
-};
-
-const findAPaymentDate = (strDate, frequency, value) => {
-  const date = new Date(strDate);
-  switch (frequency) {
-    case "yearly":
-      date.setMonth(value ? value[0] : 11);
-      date.setDate(value ? value[1] : 31);
-      if (!(date > new Date(strDate))) {
-        date.setFullYear(date.getFullYear() + 1);
-      }
-      break;
-    case "monthly":
-      value = value || daysInMonth(date.getMonth(), date.getFullYear());
-      date.setDate(value);
-      if (!(date > new Date(strDate))) {
-        date.setMonth(date.getMonth() + 1);
-      }
-      break;
-    case "fortnightly":
-    case "weekly":
-      value = value || "Sunday";
-      for (var i = 1; i <= 14; i++) {
-        date.setDate(date.getDate() + 1);
-        if (value === days[date.getDay()]) {
-          break;
-        }
-      }
-      break;
-    case "daily":
-      date.setDate(date.getDate() + 1);
-      break;
-    default:
-      return null;
-  }
-  return date;
 };
 
 const toString = date => {
