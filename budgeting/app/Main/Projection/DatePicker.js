ca54f3fc2599cb04a722931861576a9a758a8ad5
Lite W Kim <litek@orionhealth.com>
2023-10-02 00:21:35 +1300

    fix bugs

--- budgeting/app/Main/Projection/DatePicker.js
+++ budgeting/app/Main/Projection/DatePicker.js
@@ -4,10 +4,17 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { dateToString } from '@/constants/dates';
+
 
 const BasicDatePicker = props => {
   const { label, defaultDate, setDate } = props;
+
+  const toDate = djs => {
+    const year = djs.get("year");
+    const month = djs.get("month") + 1;
+    const day = djs.get("date");
+    setDate(`${year}-${month > 9 ? month : `0${month}`}-${day > 9 ? day : `0${day}`}`)
+  }
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -16,7 +23,7 @@ const BasicDatePicker = props => {
           format='YYYY-MM-DD'
           defaultValue={defaultDate ? dayjs(defaultDate) : undefined}
           label={label}
-          onChange={djs => dateToString(djs)}
+          onChange={djs => toDate(djs)}
           sx={{
             color: "white",
             "&.MuiTextField-root": { minWidth: "unset" },
